97c7c8a50a0316bf016b45b2d5f6ad484636d8ac
Hannah Newsome <h.e.newsome@gmail.com>
2021-07-01 22:55:32 -0400

    updating column names

--- clean_spells.py
+++ clean_spells.py
@@ -2,4 +2,5 @@ import numpy as np
 import pandas as pd
 data = pd.read_csv('Spells.csv',error_bad_lines=False)
 
-print(data)
+data.columns = data.columns.str.replace(" ", "-")
+data.columns = data.columns.str.lower()
